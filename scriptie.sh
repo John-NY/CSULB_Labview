ae2f6f44e7932a740333b3666a3f865fb8f7518a
Kwon Lab <donovan.csulb@gmail.com>
2012-03-27 17:14:35 -0700

    auto update

--- scriptie.sh
+++ scriptie.sh
@@ -1,3 +1,11 @@
+## first, copy all files, preserving attributes
+cp -p ../2012_March_Test/*.vi March/ 
+cp -p ../2012_March_Test/*.lv* March/
+cp -p ../2012_March_Test/*.m March/
+cp -p ../2012_March_Test/*.ali* March/
+## next, add all files and directories.
 git add *
+## set up and name the commit
 git commit -m 'auto update'
+## do the commit
 git push -u origin master
